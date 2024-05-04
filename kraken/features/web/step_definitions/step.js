624a963aa732f5eaedb1c4aad43b7f49558ccf7e
Orlando Arnedo <Orlando.Arnedo@sagepub.com>
2024-05-04 08:16:36 -0500

    edit slug basic implementation

--- kraken/features/web/step_definitions/step.js
+++ kraken/features/web/step_definitions/step.js
@@ -13,3 +13,45 @@ When(
 		await nextButton.click();
 	}
 );
+
+When("I Click on user dropdown", async function () {
+	let userDropdown = await this.driver.$(`div.gh-user-avatar`);
+	await userDropdown.click();
+});
+
+When("I click on the 'Your profile' link", async function () {
+	let profileLink = await this.driver.$(
+		"//a[contains(text(), 'Your profile')]"
+	);
+	await profileLink.click();
+});
+
+When(
+	"I modify the user name to {string} and save changes",
+	async function (newName) {
+		let nameField = await this.driver.$("input[name='user']");
+		await nameField.setValue(newName);
+
+		let saveButton = await this.driver.$("button.gh-btn-primary");
+		await saveButton.click();
+	}
+);
+
+Then(
+	"I should see the updated name {string} in the profile",
+	async function (expectedName) {
+		let profileName = await this.driver.$("h2.post-card-title");
+		let displayedName = await profileName.getText();
+		if (displayedName !== expectedName) {
+			throw new Error(
+				`Expected name to be ${expectedName} but found ${displayedName}`
+			);
+		}
+	}
+);
+
+When("I wait {int} seconds", function (seconds) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, seconds * 1000); // Convert seconds to milliseconds
+	});
+});
